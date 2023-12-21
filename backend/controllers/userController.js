8aef18450bf4db11172354627b2b667e3989abe2
mehran shiraz <meharmani212@gmail.com>
2023-12-22 01:11:04 +0500

    initial

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,5 +1,7 @@
-const authUser = (res,req) => {
+import asyncHandler from 'express-async-handler'
+
+const authUser = asyncHandler( async (req,res) => {
 res.status(200).json({message:"Auth User"})
-}
+});
 
 export {authUser};
--- backend/server.js
+++ backend/server.js
@@ -3,12 +3,15 @@ import dotenv from 'dotenv';
 dotenv.config();
 const port = process.env.PORT || 5000;
 import userRoute from './routes/userRoutes.js'
-
+import { notFound,errorHandler } from "./middleware/errorMiddleware.js";
 const app =express();
 
 app.use('/api/users',userRoute)
 
 app.get('/', (req,res) => res.send('Server is ready'));
+
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(port, () => {
     console.log(`Server Started on port ${port}`)
