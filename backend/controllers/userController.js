20402f046418e921ca8a4d45110ee6c5132c8794
mehran shiraz <meharmani212@gmail.com>
2024-01-16 23:50:41 +0500

    routes defined

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,7 +1,74 @@
 import asyncHandler from 'express-async-handler'
 
+/**
+ * @desc   Auth user/set token
+ * 
+ * route Post/api/users/auth
+ * 
+ * @access Public
+ * 
+ */
 const authUser = asyncHandler( async (req,res) => {
 res.status(200).json({message:"Auth User"})
 });
 
-export {authUser};
+/**
+ * @desc   Register new user
+ * 
+ * route Post/api/users
+ * 
+ * @access Public
+ * 
+ */
+const registerUser = asyncHandler( async (req,res) => {
+    res.status(200).json({message:"Register New User"})
+    });
+    
+
+ /**
+ * @desc   Logout User
+ * 
+ * route Post/api/users/logout
+ * 
+ * @access Public
+ * 
+ */
+const logoutUser = asyncHandler( async (req,res) => {
+    res.status(200).json({message:"Logout User"})
+    });
+
+ /**
+ * @desc   Get User Profile
+ * 
+ * route Get/api/users/profile
+ * 
+ * @access Private
+ * 
+ */
+ const getUserProfile = asyncHandler( async (req,res) => {
+    res.status(200).json({message:"Get User Profile"})
+    });
+
+
+ /**
+ * @desc   Update User Profile
+ * 
+ * route  PUT /api/users/profile
+ * 
+ * @access Private
+ * 
+ */
+ const updateUserProfile = asyncHandler( async (req,res) => {
+    res.status(200).json({message:"Update User Profile"})
+    });
+    
+    
+
+export {
+    authUser,
+    registerUser,
+    logoutUser,
+    getUserProfile,
+    updateUserProfile
+
+};
--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ dotenv.config();
 const port = process.env.PORT || 5000;
 import userRoute from './routes/userRoutes.js'
 import { notFound,errorHandler } from "./middleware/errorMiddleware.js";
+import connectDB from "./config/db.js";
 const app =express();
 
 app.use('/api/users',userRoute)
